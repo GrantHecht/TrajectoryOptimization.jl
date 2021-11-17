71796830705d25ffa41006682f69e9f70774a366
Grant Hecht <granthec@buffalo.edu>
2021-11-16 21:46:42 -0500

    Switched Snopt.jl to optional dependency because it's not free software.

--- src/TrajectoryOptimization.jl
+++ src/TrajectoryOptimization.jl
@@ -1,7 +1,11 @@
 module TrajectoryOptimization
 
-using SparseArrays, StaticArrays, LinearAlgebra
-using ForwardDiff, Snopt
+using SparseArrays
+using StaticArrays
+using LinearAlgebra
+using ForwardDiff
+using Ipopt
+using Requires
 using Symbolics: jacobian_sparsity
 
 # Utilities
@@ -10,5 +14,10 @@ include("Utilities/typeFlags.jl")
 # User function utilities
 include("UserFunctionUtilities/PathFunction.jl")
 include("UserFunctionUtilities/ADPathFunction.jl")
+
+# Conditionally load Snopt
+function __init__()
+    @require Snopt="0e9dc826-d618-11e8-1f57-c34e87fde2c0" include("NLPUtilities/snopt.jl")
+end
 
 end
