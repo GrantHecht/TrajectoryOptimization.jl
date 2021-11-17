2a992f48dad8b95e2cdf6ef449753fc3d797a481
Grant Hecht <granthec@buffalo.edu>
2021-11-17 10:55:38 -0500

    Finished adding path function Jacobian tests.

--- src/UserFunctionUtilities/PathFunction.jl
+++ src/UserFunctionUtilities/PathFunction.jl
@@ -6,6 +6,7 @@ function GetFunctionType(pf::PathFunction{type}) where {type}
     return type
 end
 
+# Evaluate functions method
 function EvaluateFunction(pf::PathFunction,
                           out::AbstractVector,
                           state::AbstractVector,
@@ -16,29 +17,41 @@ function EvaluateFunction(pf::PathFunction,
     return nothing
 end
 
+# Get number of parameters methods
 function GetNumberOfFunctions(pf::PathFunction)
     return pf.nFuncs
 end
-
 function GetNumberOfStates(pf::PathFunction)
     return pf.nStates
 end
-
 function GetNumberOfControls(pf::PathFunction)
     return pf.nControls
 end
-
 function GetNumberOfStatics(pf::PathFunction)
     return pf.nStatic
 end
 
+# Function name methods
 function SetFunctionNames!(pf::PathFunction, names::Vector{String})
     for i in 1:length(names)
         push!(pf.funcNames, name[i])
     end
     return nothing
 end
-
 function GetFunctionNames(pf::PathFunction)
     return pf.funcNames
+end
+
+# Get Jacobian Sparsity Methods
+function GetJacobianSparsity(jacType::State, pf::PathFunction)
+    return pf.stateSP 
+end
+function GetJacobianSparsity(jacType::Control, pf::PathFunction)
+    return pf.controlSP
+end
+function GetJacobianSparsity(jacType::Static, pf::PathFunction)
+    return pf.staticSP
+end
+function GetJacobianSparsity(jacType::Time, pf::PathFunction)
+    return pf.timeSP
 end
--- test/pathFunctionTests.jl
+++ test/pathFunctionTests.jl
@@ -1,7 +1,7 @@
 using TrajectoryOptimization
 using LinearAlgebra
 
-# Simple keplarian dynamics function
+# Simple keplarian dynamics with thrust acceleration ODE function
 function keplar!(out, x, u, p, t)
     # Earth gravitational parameter (likely would never be a static parameter,
     # but using here for testing purposes...)
@@ -13,6 +13,38 @@ function keplar!(out, x, u, p, t)
     # Derivatives
     @views out[1:3] .= x[4:6]
     @views out[4:6] .= -(μ / r^3).*x[1:3] .+ u
+    return nothing
+end
+
+# Analytical Jacobians
+function stateJac!(out, x, u, p, t)
+    μ = p[1]
+    out .= 0.0
+
+    # ddr/dv 
+    for i in 1:3
+        out[i, 3 + i] = 1.0
+    end
+
+    # ddv/dr
+    @views r = norm(x[1:3])
+    @views mul!(out[4:6, 1:3], x[1:3], transpose(x[1:3]))
+    @views out[4:6, 1:3] .*= (3.0 / r^2)
+    for i in 1:3; out[3 + i, i] -= 1.0; end
+    @views out[4:6, 1:3] .*= (μ/r^3)
+    return nothing
+end
+function controlJac!(out, x, u, p, t)
+    out .= 0.0
+    for i in 1:3
+        out[3 + i, i] = 1.0
+    end
+    return nothing
+end
+function staticJac!(out, x, u, p, t)
+    out .= 0.0
+    @views r = norm(x[1:3])
+    @views out[4:6] .= -(1/r^3).*x[1:3]
 end
 
 # ===== Construct AD Path Function
@@ -48,3 +80,44 @@ keplar!(outWrapped, states, controls, static, time)
 for i in 1:nFuncs; @test out[i] == outWrapped[i]; end
 
 # ===== Test Jacobian evaluation
+# State Jacobian
+stateJac        = Matrix{Float64}(undef, 6, 6)
+stateJacWrapped = Matrix{Float64}(undef, 6, 6)
+TrajectoryOptimization.EvaluateJacobian(TrajectoryOptimization.State(), 
+    adpf, stateJac, states, controls, static, time)
+stateJac!(stateJacWrapped, states, controls, static, time)
+for col in 1:6
+    for row in 1:6
+        @test stateJac[row, col] ≈ stateJacWrapped[row, col]
+    end
+end
+
+# Control Jacobian
+controlJac      = Matrix{Float64}(undef, 6, 3)
+controlJacWrap  = Matrix{Float64}(undef, 6, 3)
+TrajectoryOptimization.EvaluateJacobian(TrajectoryOptimization.Control(),
+    adpf, controlJac, states, controls, static, time)
+controlJac!(controlJacWrap, states,  controls, static, time)
+for col in 1:3
+    for row in 1:6
+        @test controlJac[row, col] ≈ controlJacWrap[row, col]
+    end
+end
+
+# Static Jacobian
+staticJac       = Matrix{Float64}(undef, 6, 1)
+staticJacWrap   = Matrix{Float64}(undef, 6, 1)
+TrajectoryOptimization.EvaluateJacobian(TrajectoryOptimization.Static(),
+    adpf, staticJac, states, controls, static, time)
+staticJac!(staticJacWrap, states, controls, static, time)
+for row in 1:6
+    @test staticJac[row, 1] ≈ staticJacWrap[row, 1]
+end
+
+# Time Jacobian
+timeJac         = Matrix{Float64}(undef, 6, 1)
+TrajectoryOptimization.EvaluateJacobian(TrajectoryOptimization.Time(),
+    adpf, timeJac, states, controls, static, time)
+for row in 1:6
+    @test timeJac[row, 1] ≈ 0.0
+end
